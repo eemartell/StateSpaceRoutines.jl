d620534d1b6476942812721416edd3a8eb8e9353
William Chen <wyc1@williams.edu>
2018-06-18 11:50:50 -0400

    Add everywhere macro to make parallel execution work.

--- docs/examples/tempered_particle_filter/tempered_particle_filter_ex.jl
+++ docs/examples/tempered_particle_filter/tempered_particle_filter_ex.jl
@@ -7,7 +7,7 @@ using DataFrames
 
 m = AnSchorfheide()
 df = readtable("us.txt", header = false, separator = ' ')
-data = convert(Matrix{Float64}, df)'
+@everywhere data = convert(Matrix{Float64}, df)'
 
 params = [2.09, 0.98, 2.25, 0.65, 0.34, 3.16, 0.51, 0.81, 0.98, 0.93, 0.19, 0.65, 0.24,
           0.115985, 0.294166, 0.447587]
@@ -24,18 +24,18 @@ DD      = system[:DD]
 ZZ      = system[:ZZ]
 QQ      = system[:QQ]
 
-Φ(s_t::Vector{Float64}, ϵ_t::Vector{Float64}) = TTT*s_t + RRR*ϵ_t + CCC
-Ψ(s_t::Vector{Float64}, u_t::Vector{Float64}) = ZZ*s_t + DD + u_t
+@everywhere Φ(s_t::Vector{Float64}, ϵ_t::Vector{Float64}) = TTT*s_t + RRR*ϵ_t + CCC
+@everywhere Ψ(s_t::Vector{Float64}, u_t::Vector{Float64}) = ZZ*s_t + DD + u_t
 
-F_ϵ = Distributions.MvNormal(zeros(size(QQ, 1)), QQ)
-F_u = Distributions.MvNormal(zeros(size(HH, 1)), HH)
+@everywhere F_ϵ = Distributions.MvNormal(zeros(size(QQ, 1)), QQ)
+@everywhere F_u = Distributions.MvNormal(zeros(size(HH, 1)), HH)
 
 # Tuning of the tempered particle filter algorithm
 
-tuning = Dict(:verbose => :none, :r_star => 2., :c => 0.3, :accept_rate => 0.4, :target => 0.4,
+@everywhere tuning = Dict(:verbose => :none, :r_star => 2., :c => 0.3, :accept_rate => 0.4, :target => 0.4,
                :N_MH => 1,
               :n_particles => 1000, :n_presample_periods => 0,
-               :allout => true, :parallel => false, :sharedarrays => false)
+               :allout => true, :parallel => true, :sharedarrays => false)
 
 # Generation of the initial state draws
 
@@ -43,5 +43,5 @@ n_states = n_states_augmented(m)
 s0 = zeros(n_states)
 P0 = solve_discrete_lyapunov(TTT, RRR*QQ*RRR')
 U, E, V = svd(P0)
-s_init = s0 .+ U*diagm(sqrt.(E))*randn(n_states, tuning[:n_particles])
+@everywhere s_init = s0 .+ U*diagm(sqrt.(E))*randn(n_states, tuning[:n_particles])
 tempered_particle_filter(data, Φ, Ψ, F_ϵ, F_u, s_init; tuning...)
